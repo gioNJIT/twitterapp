c92c0a2bdb84fb2eb8444be75ddc5a44b3725fdd
Giovanni DeRosa <gd263@njit.edu>
2021-10-11 20:20:44 +0000

    added microsft api. currently testing key phrase extraction. will work on sending tweets back to front end and will ask user which api function to use on the tweets.

--- README.md
+++ README.md
@@ -9,6 +9,11 @@
 5. `sudo yum install postgresql postgresql-server postgresql-devel postgresql-contrib postgresql-docs`
 6. `pip install psycopg2-binary`
 7. `pip install Flask-SQLAlchemy==2.1`
+8. `pip install azure-ai-textanalytics==5.2.0b1`
+
+## API's used
+1. Microsoft textanalytics
+2. twitter API
 
 ## Setup
 1. Run `echo "DANGEROUSLY_DISABLE_HOST_CHECK=true" > .env.development.local` in the project directory
@@ -16,6 +21,7 @@
 3. `sudo service postgresql start` start postgresql
 4. `sudo -u postgres createuser --superuser $USER`
 5. `sudo -u postgres createdb $USER`
+
 
 ## Run Application
 1. Run command in terminal (in your project directory): `python app.py`
--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import requests
 from flask import Flask, request, redirect, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
 
 app = Flask(__name__, static_folder='./build/static')
@@ -24,21 +26,61 @@ import models
 def index(filename):
     return send_from_directory('./build', filename)
 
-
+#twitter api call
 @app.route('/Twitterapi', methods=['POST'])
 def twitteruser():
+        
+        #what we're getting from react page
         username = request.json['username']
         
+        #setting up the url and headers
         url='https://api.twitter.com/2/users/by/username/' + username
         headers = {'Authorization': "Bearer "+os.getenv('TWITTER_TOKEN') }
-
+        
+        #sending and receiving data from twitter to get the user id #
         id_r = requests.get(url,headers=headers)
         response_id = id_r.json()
-        print(response_id['data']['id'])
+        
+        #making another call to twitter with the user id # in order to get the tweets
         url2='https://api.twitter.com/2/users/'+response_id['data']['id']+'/tweets'
-
         tweets_r=requests.get(url2,headers=headers)
-        print(tweets_r.json())
+        tweetdata=tweets_r.json()
+        tweetsAnd_ids={}
+        #creating a loop to extract the tweets and the ids associated with them
+        
+        for x in range(len(tweetdata['data'])):
+            tweetsAnd_ids[tweetdata['data'][x]['id']]=tweetdata['data'][x]['text']
+            print(tweetdata['data'][x]['text'])
+        
+        
+        #MICROSOFT API   
+        micro_key=os.getenv('MICROKEY')
+        endpoint='https://twitter-bot.cognitiveservices.azure.com/'
+        
+        #creatiing credentials to be used when accessing microsoft API
+        ta_credential = AzureKeyCredential(micro_key)
+        text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential)
+        
+        
+        # testing out "key phrase extraction" function in the text analytics api.
+        try:
+            documents = [tweetdata['data'][x]['text']]
+
+            response = text_analytics_client.extract_key_phrases(documents = documents)[0]
+
+            if not response.is_error:
+                print("\tKey Phrases:")
+                for phrase in response.key_phrases:
+                    print("\t\t", phrase)
+            else:
+                print(response.id, response.error)
+
+        except Exception as err:
+            print("Encountered exception. {}".format(err))
+
+        
 	
         return('ok')
 
